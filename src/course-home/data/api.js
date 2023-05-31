1307350570398ec1d0b01d2038733a43d09159af
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-05-31 09:56:37 +0700

    Update api.js

--- src/course-home/data/api.js
+++ src/course-home/data/api.js
@@ -113,7 +113,7 @@ function normalizeCourseHomeCourseMetadata(metadata, rootSlug) {
 }
 
 export function normalizeOutlineBlocks(courseId, blocks) {
-  console.log(blocks);
+  console.log('blocks',blocks);
   const models = {
     courses: {},
     sections: {},
