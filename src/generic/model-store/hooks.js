32eae5297e23f64762d80e93ffdba6fbc2993821
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-05-31 07:58:39 +0700

    Update hooks.js

--- src/generic/model-store/hooks.js
+++ src/generic/model-store/hooks.js
@@ -4,7 +4,7 @@ import { useSelector, shallowEqual } from 'react-redux';
   Return the selected model with the given id, or an empty object if the model does not exist "{}".
  */
 export function useModel(type, id) {
-  console.log(useSelector(state=>state.models[type][id]))
+  console.log(useSelector(state=>state.models['outline']))
   return useSelector(
     state => ((state.models[type] !== undefined && state.models[type][id] !== undefined) ? state.models[type][id] : {}),
     shallowEqual,
