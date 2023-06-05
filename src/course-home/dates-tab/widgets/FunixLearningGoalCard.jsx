ccc9514a9c3afc393f7a466d1e976ce388f8c05d
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-06-05 12:55:35 +0700

    a

--- src/course-home/dates-tab/widgets/FunixLearningGoalCard.jsx
+++ src/course-home/dates-tab/widgets/FunixLearningGoalCard.jsx
@@ -81,11 +81,11 @@ function FunixLearningGoalCard({
      
       <Card.Body className="p-3 p-lg-3.5">
         <h2 className="h4 mb-1 text-primary-500">{intl.formatMessage(messages.setHourDailyText)}</h2>
-        <Card.Text
+        {/* <Card.Text
           className="text-gray-700 small mb-2.5"
-        >
-          {/* {intl.formatMessage(messages.setHourDailyDetail)} */}
-        </Card.Text>
+        > */}
+          {intl.formatMessage(messages.setHourDailyDetail)}
+        {/* </Card.Text> */}
         {/* <Input
           min="1"
           max="24"
