032092ae706bf10428bd63beaaa26aacbf5a32bc
sonxauxi2411 <112046655+sonxauxi2411@users.noreply.github.com>
2023-08-07 23:12:41 +0700

    check logs

--- src/course-home/data/api.js
+++ src/course-home/data/api.js
@@ -473,7 +473,7 @@ export async function getSection(courseId){
   const courseBlocks = data.course_blocks ? normalizeOutlineBlocks(courseId, data.course_blocks.blocks) : {}
   
   const filteredData = Object.fromEntries(
-    Object.entries(courseBlocks.sections).filter(([key, value]) => value.title !== "Giới Thiệu" && value.title !== 'Kết thúc')
+    Object.entries(courseBlocks.sections).filter(([key, value]) => value.title !== "Giới Thiệu" && value.title !== 'Kết thúc' && value.title !=='Mở đầu' , values.title !== 'Kết thúc khóa học')
   )
   const newData =  Object.values(filteredData)
   
